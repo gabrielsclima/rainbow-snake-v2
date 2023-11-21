b70afe95cb803c6b6cf480c73a291d26116bd1c0
root <root@pop-os.localdomain>
2023-11-21 15:32:23 -0300

    That loop was just used because of the random library. Removing it as well.

--- snake.py
+++ snake.py
@@ -1,4 +1,4 @@
-#Libraries
+# Libraries
 from time import sleep
 from keyboard import is_pressed
 
@@ -11,21 +11,26 @@ P = "\033[0;35m█" #Purple
 END = "\033[0m"   #End of color
 
 # Rainbow settings
-RB = (R, Y, G, C, P) #Rainbow
-SNAKE_SIZE = 5
-POS_MIN = 0 #Minimum position value
-POS_MAX = int(input("How much the snake can slide on your screen: ")) #Maximum position value
-currentPos = POS_MAX // 2 #Makes the snake start in the middle of the terminal, according to the POS_MAX declared by the user
+RB = (R, Y, G, C, P) # Rainbow colors.
+SNAKE_SIZE = 5 # Snake... size.
+POS_MIN = 0 # Minimum position value.
+POS_MAX = int(input("How much the snake can slide on your screen: ")) # Maximum position value. 
 
+# Obs: "Position" stands for the number of space characters 
+# that will be printed before the rainbow row.
+currentPos = POS_MAX // 2
+
+# Get ready for fun!
 while True:
-    for i in range(5):
-        if is_pressed('left') and currentPos > POS_MIN:
-                currentPos -= 1
-        elif is_pressed('right') and currentPos < POS_MAX:
-             currentPos += 1
-        print(" " * currentPos, end="")
-        for j in range(SNAKE_SIZE): #
-            print(RB[(currentPos + j) % 5], end="")
-            print(RB[(currentPos + j) % 5], end="")
-        print(END)
-        sleep(0.02)
+    if is_pressed('left') and currentPos > POS_MIN:
+            currentPos -= 1
+    elif is_pressed('right') and currentPos < POS_MAX:
+            currentPos += 1
+    print(" " * currentPos, end="")
+
+    for i in range(SNAKE_SIZE): #
+        print(RB[(currentPos + j) % 5], end="")
+        print(RB[(currentPos + j) % 5], end="")
+
+    print(END)
+    sleep(0.02)
